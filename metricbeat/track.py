7f6391c9eaf5e7d3bedcc8804361afddee76c66c
Joe Gallo <joegallo@gmail.com>
2025-07-31 14:57:35 -0400

    Initial shot at dls-searching

--- metricbeat/track.py
+++ metricbeat/track.py
@@ -1,3 +1,6 @@
+import random
+import threading
+
 from esrally import exceptions
 
 async def reindex(es, params):
@@ -31,13 +34,13 @@ async def put_roles_and_users(es, params):
         )
 
     for idx, roles in enumerate([
-            ["debian"],
+            ["debian"], # user_0
             ["redhat"],
             ["suse"],
             ["debian","redhat"],
             ["debian","suse"],
             ["redhat","suse"],
-            ["debian","redhat","suse"]
+            ["debian","redhat","suse"] # user_6
     ]):
        await es.security.put_user(
             username="user_" + str(idx),
@@ -45,9 +48,105 @@ async def put_roles_and_users(es, params):
             roles=roles
         )
 
+counter = 0
+lock = threading.Lock()
+
+def get_and_increment():
+    global counter
+    result = -1
+    with lock:
+        result = counter
+        counter += 1
+    return result
+
+async def dls_search(es, params):
+    c = get_and_increment()
+
+    # we want some cache cycling, but also some entries that always just stay in the cache, so:
+    # 25% of the time, use user_0, 75% of the time, use user_1 through user_6
+    uid = random.randint(1, 6)
+    if c % 4 == 0:
+        uid = 0
+
+    query = random.randint(0, 4)
+
+    if query == 0:
+        await es.options(basic_auth=("user_" + str(uid), "password")).search(
+            size=0,
+            track_total_hits=True,
+            index="metricbeat*",
+            query={"bool": {
+                "must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "core"}}
+                ]
+            }},
+            aggs={"core_system_pct": {
+                "percentiles": {"field": "system.core.system.pct"}
+            }})
+    elif query == 1:
+        await es.options(basic_auth=("user_" + str(uid), "password")).search(
+            size=0,
+            track_total_hits=True,
+            index="metricbeat*",
+            query={"bool": {
+                "must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "network"}}
+                ]
+            }},
+            aggs={"network_in_bytes": {
+                "percentiles": {"field": "system.network.in.bytes"}
+            }})
+    elif query == 2:
+        await es.options(basic_auth=("user_" + str(uid), "password")).search(
+            size=0,
+            track_total_hits=True,
+            index="metricbeat*",
+            query={"bool": {
+                "must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "process"}}
+                ]
+            }},
+            aggs={"processor_memory_size": {
+                "percentiles": {"field": "system.process.memory.size"}
+            }})
+    elif query == 3:
+        await es.options(basic_auth=("user_" + str(uid), "password")).search(
+            size=0,
+            track_total_hits=True,
+            index="metricbeat*",
+            query={"bool": {
+                "must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "filesystem"}}
+                ]
+            }},
+            aggs={"filesystem_free": {
+                "percentiles": {"field": "system.filesystem.free"}
+            }})
+    elif query == 4:
+        await es.options(basic_auth=("user_" + str(uid), "password")).search(
+            size=0,
+            track_total_hits=True,
+            index="metricbeat*",
+            query={"bool": {
+                "must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "memory"}}
+                ]
+            }},
+            aggs={"memory_total": {
+                "percentiles": {"field": "system.memory.total"}
+            }})
+    else:
+        raise Exception("Invalid query!")
+
 def register(registry):
     registry.register_runner("reindex", reindex, async_runner=True)
     registry.register_runner("put-roles-and-users", put_roles_and_users, async_runner=True)
+    registry.register_runner("dls-search", dls_search, async_runner=True)
 
 def mandatory(params, key, op):
     try:
