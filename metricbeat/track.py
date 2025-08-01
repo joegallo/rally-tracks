45af2c0a507f8af329dfe7a9c13dfa4f4e9cddc1
Joe Gallo <joegallo@gmail.com>
2025-08-01 10:34:10 -0400

    Blowing up the size of the cache

--- metricbeat/track.py
+++ metricbeat/track.py
@@ -59,69 +59,100 @@ def get_and_increment():
         counter += 1
     return result
 
-queries_and_aggs=[
-    [{"bool": {
-        "must": [
-            {"term": {"metricset.module": "system"}},
-            {"term": {"metricset.name": "core"}}
-        ]}},
-     {"core_system_pct": {
-         "percentiles": {"field": "system.core.system.pct"}}}],
-    [{"bool": {
-        "must": [
-            {"term": {"metricset.module": "system"}},
-            {"term": {"metricset.name": "network"}}
-        ]}},
-     {"network_in_bytes": {
-         "percentiles": {"field": "system.network.in.bytes"}}}],
-    [{"bool": {
-        "must": [
-            {"term": {"metricset.module": "system"}},
-            {"term": {"metricset.name": "process"}}
-        ]}},
-     {"processor_memory_size": {
-         "percentiles": {"field": "system.process.memory.size"}}}],
-    [{"bool": {
-        "must": [
-            {"term": {"metricset.module": "system"}},
-            {"term": {"metricset.name": "filesystem"}}
-        ]}},
-     {"filesystem_free": {
-         "percentiles": {"field": "system.filesystem.free"}}}],
-    [{"bool": {
-        "must": [
-            {"term": {"metricset.module": "system"}},
-            {"term": {"metricset.name": "memory"}}
-        ]}},
-     {"memory_total": {
-         "percentiles": {"field": "system.memory.total"}}}]
-]
+def queries_and_aggs(qidx, rand):
+    big = rand * 128
+    little = rand * 0.125
+    if qidx == 0:
+        return [
+            {
+                "bool": {"must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "core"}},
+                    {"range" : {"system.core.system.pct" : {"gte" : little}}}
+                ]}
+            },
+            {
+                "core_system_pct": {"percentiles": {"field": "system.core.system.pct"}}
+            }
+        ]
+    elif qidx == 1:
+        return [
+            {
+                "bool": {"must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "network"}},
+                    {"range" : {"system.network.in.bytes" : {"gte" : big}}}
+                ]}
+            },
+            {
+                "network_in_bytes": {"percentiles": {"field": "system.network.in.bytes"}}
+            }
+        ]
+    elif qidx == 2:
+        return [
+            {
+                "bool": {"must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "process"}},
+                    {"range" : {"system.process.memory.size" : {"gte" : big}}}
+                ]}
+            },
+            {
+                "processor_memory_size": {"percentiles": {"field": "system.process.memory.size"}}
+            }
+        ]
+    elif qidx == 3:
+        return [
+            {
+                "bool": {"must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "filesystem"}},
+                    {"range" : {"system.filesystem.free" : {"gte" : big}}}
+                ]}
+            },
+            {
+                "filesystem_free": {"percentiles": {"field": "system.filesystem.free"}}
+            }
+        ]
+    elif qidx == 4:
+        return [
+            {
+                "bool": {"must": [
+                    {"term": {"metricset.module": "system"}},
+                    {"term": {"metricset.name": "memory"}},
+                    {"range" : {"system.memory.total" : {"gte" : big}}}
+                ]}
+            },
+            {
+                "memory_total": {"percentiles": {"field": "system.memory.total"}}
+            }
+        ]
+
 
 async def dls_search(es, params):
     c = get_and_increment()
 
     # we want some cache cycling, but also some entries that always just stay in the cache, so:
-    # 25% of the time, use user_0, 75% of the time, use user_1 through user_6
+    # 40% of the time, use user_0, 60% of the time, use user_1 through user_6
     uid = random.randint(1, 6)
-    if c % 4 == 0:
+    if c % 10 <= 4:
         uid = 0
 
     qidx = random.randint(0, 4)
-    cost = random.randint(0, 10)
+    filter_value = random.randint(0, 8)
+    query, agg = queries_and_aggs(qidx, filter_value)
 
+    cost = random.randint(0, 10)
     size = 0
     track_total_hits=False
-    query = queries_and_aggs[qidx][0]
-    agg = queries_and_aggs[qidx][1]
-
-    if cost == 10:
-        size = 100
-
     if cost >= 3:
         track_total_hits=True
 
     if cost <= 6:
         agg = None
+
+    if cost == 10:
+        size = 100
 
     await es.options(basic_auth=("user_" + str(uid), "password")).search(
         index="metricbeat*",
